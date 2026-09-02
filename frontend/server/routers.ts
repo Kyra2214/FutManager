fffc9feb456f7a221fd7dd36934075033841b5ba
Alexandre <xandelsw@gmail.com>
2026-09-02 17:57:00 -0300

    feat: register multi-save router

--- frontend/server/routers.ts
+++ frontend/server/routers.ts
@@ -11,6 +11,7 @@ import { staffMarketRouter } from "./routers/staffMarket";
 import { sponsorshipRouter } from "./routers/sponsorship";
 import { stadiumRouter } from "./routers/stadium";
 import { operationsRouter } from "./routers/operations";
+import { savesRouter } from "./routers/saves";
 
 export const appRouter = router({
     // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
@@ -34,6 +35,7 @@ export const appRouter = router({
   sponsorship: sponsorshipRouter,
   stadium: stadiumRouter,
   operations: operationsRouter,
+  saves: savesRouter,
 
   // TODO: add feature routers here, e.g.
   // todo: router({
